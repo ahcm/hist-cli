626115de997db8b59c0f3bf2b277aa04abdcf0ba
Andreas Hauser <andreas.github@icloud.com>
2024-09-16 00:11:12 +0200

    Format with rustfmt

--- src/bin/hist.rs
+++ src/bin/hist.rs
@@ -1,17 +1,21 @@
-extern crate plotters;
 extern crate csv;
+extern crate plotters;
 use csv::StringRecord;
 use plotters::prelude::*;
-use std::io;
 use std::collections::BTreeMap;
-use structopt::StructOpt;
-use std::path::PathBuf;
 use std::error::Error;
 use std::fs::File;
+use std::io;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 #[allow(non_snake_case)]
 #[derive(Debug, StructOpt)]
-#[structopt(name = "hist", about = "Plots histogram of input", rename_all="verbatim")]
+#[structopt(
+    name = "hist",
+    about = "Plots histogram of input",
+    rename_all = "verbatim"
+)]
 struct Opt
 {
     #[structopt(parse(from_os_str))]
@@ -68,22 +72,25 @@ fn main() -> Result<(), Box<dyn Error>>
 {
     let opt = Opt::from_args();
 
-    let input: Box<dyn std::io::Read + 'static> =
-        if let Some(path) = &opt.input
-        {
-            Box::new(File::open(&path).unwrap())
-        }
-        else
-        {
-            Box::new(io::stdin())
-        };
+    let input: Box<dyn std::io::Read + 'static> = if let Some(path) = &opt.input
+    {
+        Box::new(File::open(&path).unwrap())
+    }
+    else
+    {
+        Box::new(io::stdin())
+    };
 
     // accept escaped delimiters
     // could be expanded to aliases e.g. "TAB"
     let delimiter = match opt.delimiter.as_str()
     {
         r"\t" => b'\t', // structopt needs r"" to show default as escaped, also for sepcifiying as escaped in CLI
-         _ => *opt.delimiter.as_bytes().first().expect("Not a valid delimiter")
+        _ => *opt
+            .delimiter
+            .as_bytes()
+            .first()
+            .expect("Not a valid delimiter"),
     };
 
     let mut reader = csv::ReaderBuilder::new()
@@ -95,8 +102,8 @@ fn main() -> Result<(), Box<dyn Error>>
     let mut record = StringRecord::new();
     while reader.read_record(&mut record)?
     {
-       let s = record.get(opt.key - 1).unwrap().to_string(); 
-       key_counts.entry(s).and_modify(|e| *e += 1 ).or_insert(1);
+        let s = record.get(opt.key - 1).unwrap().to_string();
+        key_counts.entry(s).and_modify(|e| *e += 1).or_insert(1);
     }
 
     if let Some(path) = &opt.save
@@ -104,7 +111,10 @@ fn main() -> Result<(), Box<dyn Error>>
         save(&key_counts, &path);
     }
 
-    let mut sorted_counts = key_counts.values().fold(Vec::new(), |mut v, x| { v.push(*x); v });
+    let mut sorted_counts = key_counts.values().fold(Vec::new(), |mut v, x| {
+        v.push(*x);
+        v
+    });
     sorted_counts.sort();
 
     if opt.textplot
@@ -123,42 +133,44 @@ fn main() -> Result<(), Box<dyn Error>>
     }
 }
 
-fn save(counts : &BTreeMap<String, usize>, path : &std::path::Path)
+fn save(counts: &BTreeMap<String, usize>, path: &std::path::Path)
 {
-    let mut out: Box<dyn std::io::Write + 'static> =
-        if path == std::path::Path::new("-")
-        {
-            Box::new(io::stdout())
-        }
-        else
-        {
-            Box::new(File::create(&path).unwrap())
-        };
+    let mut out: Box<dyn std::io::Write + 'static> = if path == std::path::Path::new("-")
+    {
+        Box::new(io::stdout())
+    }
+    else
+    {
+        Box::new(File::create(&path).unwrap())
+    };
 
     let mut entries = Vec::from_iter(counts);
     entries.sort_by(|&(_, a), &(_, b)| a.cmp(&b)); // sort by value
 
-
-    for (key, count) in entries 
+    for (key, count) in entries
     {
-       out.write_fmt(format_args!("{}\t{}\n", count, key)).expect("Write to save file failed");
+        out.write_fmt(format_args!("{}\t{}\n", count, key))
+            .expect("Write to save file failed");
     }
 }
 
-const BLUE : plotters::style::RGBColor = RGBColor(0x2a, 0x71, 0xb0);
+const BLUE: plotters::style::RGBColor = RGBColor(0x2a, 0x71, 0xb0);
 
-fn next_potence(x : f64) -> f64
+fn next_potence(x: f64) -> f64
 {
     10f64.powf(((x.log10() * 10f64).ceil()) / 10.0)
 }
 
-fn plot_rank(sorted_counts : &Vec<usize>, opt : &Opt) -> Result<(), Box<dyn Error>>
+fn plot_rank(sorted_counts: &Vec<usize>, opt: &Opt) -> Result<(), Box<dyn Error>>
 {
     let max = *sorted_counts.last().expect("At lease one entry is needed");
     let y_dim = next_potence(max as f64) as usize;
     let x_dim = (sorted_counts.len() as f64 * 1.1) as usize;
 
-    let (geometry_x_str, geometry_y_str) = opt.geometry.split_once("x").expect("geometry not in correct format");
+    let (geometry_x_str, geometry_y_str) = opt
+        .geometry
+        .split_once("x")
+        .expect("geometry not in correct format");
     let geometry_x = geometry_x_str.parse().expect("Unable to parse geometry x");
     let geometry_y = geometry_y_str.parse().expect("Unable to parse geometry y");
     let root = BitMapBackend::new(&opt.output, (geometry_x, geometry_y)).into_drawing_area();
@@ -182,32 +194,30 @@ fn plot_rank(sorted_counts : &Vec<usize>, opt : &Opt) -> Result<(), Box<dyn Erro
         .axis_desc_style(("sans-serif", 24))
         .draw()?;
 
-    chart.draw_series(
-        sorted_counts.iter().rev().enumerate().map(|(x,y)|
-                                                   {
-                                                       let x0 = SegmentValue::Exact(x);
-                                                       let x1 = SegmentValue::Exact(x + 1);
-                                                       let mut bar = Rectangle::new([(x0, *y as usize), (x1, 0 as usize)], BLUE.filled());
-                                                       bar.set_margin(0, 0, 0, 0);
-                                                       bar
-                                                   })
-        )?;
-
+    chart.draw_series(sorted_counts.iter().rev().enumerate().map(|(x, y)| {
+        let x0 = SegmentValue::Exact(x);
+        let x1 = SegmentValue::Exact(x + 1);
+        let mut bar = Rectangle::new([(x0, *y as usize), (x1, 0 as usize)], BLUE.filled());
+        bar.set_margin(0, 0, 0, 0);
+        bar
+    }))?;
 
     Ok(())
 }
 
-fn text_plot(sorted_counts : &Vec<usize>, width : u32, height : u32, xmin : f32, xmax : f32)
+fn text_plot(sorted_counts: &Vec<usize>, width: u32, height: u32, xmin: f32, xmax: f32)
 {
-    use textplots::{Chart,Plot,Shape};
+    use textplots::{Chart, Plot, Shape};
     let max = *sorted_counts.last().expect("At lease one entry is needed");
     let y_dim = next_potence(max as f64) as f32;
     Chart::new_with_y_range(width, height, xmin, xmax, 0.0, y_dim)
         .lineplot(&Shape::Bars(
-                &sorted_counts
+            &sorted_counts
                 .iter()
                 .rev()
                 .enumerate()
-                .map(|(x,y)| ((x + 1) as f32, *y as f32)).collect::<Vec<(f32,f32)>>()
-                )).display();
+                .map(|(x, y)| ((x + 1) as f32, *y as f32))
+                .collect::<Vec<(f32, f32)>>(),
+        ))
+        .display();
 }
